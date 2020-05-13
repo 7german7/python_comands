72e583bfd7297f895d83ce1c1c95bf16df7a524e
enrique <german.junior93@gmail.com>
2020-05-13 17:32:36 -0400

    practica creando y modificando listas

---
+++ practicas/listas.py
@@ -0,0 +1,8 @@
+#********************************************************PRACTICANDO CON LISTAS O ARREGLOS********************************************************
+# Python nos permite el uso de listas o arreglos, las cuales pueden contener diferentes tipos de datos incluyendose a si misma (una lista puede contener un elemento o datos tipo lista).
+# Sintaxis: lista = ["German", 27, True, ["Estudiante", "Platzi"]]
+
+arreglo = ["German", 27, True, ["Estudiante", "Platzi", 10]]
+print arreglo[3][2] #imprime el 3er elemento de la lista interna del 4to elemento de la lista externa "arreglo"
+print arreglo[0] #imprime el 1er elemento de la lista
+print arreglo # imprime todos los datos de la lista
