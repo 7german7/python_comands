85d9ba76ff9a4a5df17bf5ff2ca43e03cc7bc0e9
enrique <german.junior93@gmail.com>
2020-05-14 10:56:59 -0400

    se modifico el archivo porque tenia un error

--- practicas/bucles.py
+++ practicas/bucles.py
@@ -6,6 +6,7 @@ for i in range(3):
     print "el valor de i usando for es: %i" % (i)
 
 # utilizando while
-n = 0
-for n in range(5):
+n = 4
+while (n>0):
     print "el valor de i usando while es: %i" % (n)
+    n-=1 # decremento es lo mismo que colocar n = n-1, para incremento podemos utilizar n+=1
